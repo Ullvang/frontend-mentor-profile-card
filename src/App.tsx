b37457bdf4fdfdf309ea46ba1fb7056d11b8494d
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-21 18:40:17 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <main>
+      Victor Crest 26 London 80K Followers 803K Likes 1.4K Photos
+      <div className="attribution">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by <a href="https://github.com/Ullvang">Jon Erik Ullvang</a>.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </main>
   );
 }
 
